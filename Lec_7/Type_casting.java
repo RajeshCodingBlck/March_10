4543fd150b9be70ce6e7048e126aed8c131f2611
RajeshCodingBlck <137406636+RajeshCodingBlck@users.noreply.github.com>
2024-11-10 14:45:03 +0530

    Add files via upload

---
+++ Lec_7/Type_casting.java
@@ -0,0 +1,76 @@
+package Lec_7;
+
+public class Type_casting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     
+//	  int a=35;
+//	  double d= a;
+//	  System.out.println(d);
+//	  System.out.println(a);
+	  
+//	  int a=34;
+//	  byte b= (byte)a;
+//	  System.out.println(b);
+	  
+//	  int a=340;
+//	  byte b= (byte)a;
+//	  System.out.println(b);
+	  
+		
+//	 int a= 23;
+//	 long b= 34;
+//	 
+//	 int c= (int)(a+b);
+//   System.out.println(c);
+	  
+//	 byte a=12;
+//	 byte b=34;
+//	 
+//	 byte c= (byte)(a+b);
+//	 System.out.println(c);
+	 
+	 
+//	 char ch='a';
+//	 System.out.println(ch);
+//	 
+//	 char ch1='b';
+//	 char ch2= (char)(ch+ch1);
+//	 System.out.println(ch2);
+
+		
+//	char ch= 'a';
+//	ch=(char)(ch+1);
+//	System.out.println(ch);
+//	
+//   ch=(char)(ch+1);
+//	ch++;
+//	System.out.println(ch);
+	
+		
+//	boolean b= true;
+//	System.out.println(b);
+	
+	 
+		// Literals 
+//		
+//		int a=23;
+//		
+//	    long b=10000000000000L;
+	    
+	   // int a= 0x1011;
+	    
+//	    int a= 100_000_00;
+//	    
+//	    System.out.println(a);
+		
+		
+		float f= 2.3F;
+		
+	    
+	 
+	  
+	}
+
+}
